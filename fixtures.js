ca69517d523b2eaaa2785a0e9d03b15108928610
tunnckoCore <mameto_100@mail.bg>
2015-01-28 05:46:39 +0200

    improve coverage to 100%

--- fixtures.js
+++ fixtures.js
@@ -2,11 +2,14 @@
 module.exports.fixtureOne = function() {
   return {
     fn: function testing(cmd, args, opts) {
+      /* istanbul ignore next */
       var obj = {
         maybe: 'yea'
       };
+      /* istanbul ignore next */
       var come = function come() { return true; };
 
+      /* istanbul ignore next */
       return {
         only: 'one',
         command: cmd,
@@ -21,14 +24,17 @@ module.exports.fixtureOne = function() {
 module.exports.fixtureTwo = function() {
   return {
     fn: function typeOf(val) {
+      /* istanbul ignore next */
       if (typeof val !== 'object') {
         return typeof val;
       }
 
+      /* istanbul ignore next */
       if (Array.isArray(val)) {
         return 'array';
       }
 
+      /* istanbul ignore next */
       return {}.toString(val).slice(8, -1).toLowerCase();
     },
     body: '\n      if (typeof val !== \'object\') {\n        return typeof val;\n      }\n\n      if (Array.isArray(val)) {\n        return \'array\';\n      }\n\n      return {}.toString(val).slice(8, -1).toLowerCase();\n    '
